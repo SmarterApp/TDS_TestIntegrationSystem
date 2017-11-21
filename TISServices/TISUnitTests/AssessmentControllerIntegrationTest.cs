6c21ed4ce6a2b834f16e879764e0871150984244
Jeff Johnson <jeffjohnson9046@hotmail.com>
2017-11-21 11:43:31 -0800

    TDS-1162:  Add verification to integration test

--- TISServices/TISUnitTests/AssessmentControllerIntegrationTest.cs
+++ TISServices/TISUnitTests/AssessmentControllerIntegrationTest.cs
@@ -28,14 +28,34 @@ namespace TISUnitTests
     public class AssessmentControllerIntegrationTest
     {
         private readonly String[] testPackageFiles = {
-            "../../resources/test-packages/tds/administration/(SBAC_PT)SBAC-IRP-CAT-MATH-3-Summer-2015-2016-UNIT-TEST.xml",
-            "../../resources/test-packages/tds/administration/(SBAC_PT)SBAC-IRP-Perf-MATH-3-Summer-2015-2016-UNIT-TEST.xml",
-            "../../resources/test-packages/tis/administration/(SBAC_PT)SBAC-IRP-MATH-3-COMBINED-Summer-2015-2016-UNIT-TEST.xml",
-            "../../resources/test-packages/tis/scoring/(SBAC_PT)SBAC-IRP-MATH-3-COMBINED-Summer-2015-2016-UNIT-TEST.xml"
+           @"..\..\resources\test-packages\tds\administration\(SBAC_PT)SBAC-IRP-CAT-MATH-3-Summer-2015-2016-UNIT-TEST.xml",
+           @"..\..\resources\test-packages\tds\administration\(SBAC_PT)SBAC-IRP-Perf-MATH-3-Summer-2015-2016-UNIT-TEST.xml",
+           @"..\..\resources\test-packages\tis\administration\(SBAC_PT)SBAC-IRP-MATH-3-COMBINED-Summer-2015-2016-UNIT-TEST.xml",
+           @"..\..\resources\test-packages\tis\scoring\(SBAC_PT)SBAC-IRP-MATH-3-COMBINED-Summer-2015-2016-UNIT-TEST.xml"
         };
 
         private const String TEST_PACKAGE_KEY = "(SBAC_PT)SBAC-IRP-MATH-3-COMBINED-Summer-2015-2016-UNIT-TEST";
 
+        private readonly String[] combinationTestMapSqls =
+        {
+            @"INSERT INTO OSS_TIS.dbo.CombinationTestMap(ComponentTestName, ComponentSegmentName, CombinationTestName, CombinationSegmentName) VALUES('(SBAC_PT)SBAC-IRP-CAT-MATH-3-Summer-2015-2016-UNIT-TEST', '(SBAC_PT)SBAC-IRP-CAT-MATH-3-Summer-2015-2016-UNIT-TEST', '(SBAC_PT)SBAC-IRP-MATH-3-COMBINED-Summer-2015-2016-UNIT-TEST', '(SBAC_PT)SBAC-IRP-CAT-COMBINED-MATH-3-Summer-2015-2016-UNIT-TEST')",
+            @"INSERT INTO OSS_TIS.dbo.CombinationTestMap(ComponentTestName, ComponentSegmentName, CombinationTestName, CombinationSegmentName) VALUES('(SBAC_PT)SBAC-IRP-Perf-MATH-3-Summer-2015-2016-UNIT-TEST', '(SBAC_PT)SBAC-IRP-Perf-MATH-3-Summer-2015-2016-UNIT-TEST', '(SBAC_PT)SBAC-IRP-MATH-3-COMBINED-Summer-2015-2016-UNIT-TEST', '(SBAC_PT)SBAC-IRP-CAT-COMBINED-MATH-3-Summer-2015-2016-UNIT-TEST' )"
+        };
+
+        private readonly String[] combinationTestFormMapSqls =
+        {
+            @"INSERT INTO OSS_TIS.dbo.CombinationTestFormMap(ComponentSegmentName, ComponentFormKey, CombinationFormKey) VALUES('(SBAC_PT)SBAC-IRP-Perf-MATH-3-Summer-2015-2016-UNIT-TEST', '187-764', '187-780')"
+        };
+
+        private readonly String[] validationSqls =
+        {
+            @"SELECT COUNT(*) FROM OSS_Itembank.dbo.tblSetOfAdminSubjects WHERE _Key LIKE '%unit-test%'",
+            @"SELECT COUNT(*) FROM OSS_TIS.dbo.CombinationTestMap WHERE CombinationTestName LIKE '%unit-test%'",
+            @"SELECT COUNT(*) FROM OSS_TIS.dbo.CombinationTestFormMap WHERE ComponentSegmentName LIKE '%unit-test%'",
+            @"SELECT COUNT(*) FROM OSS_Itembank.dbo.tblAdminStimulus WHERE _fk_AdminSubject LIKE '%unit-test%'",
+            @"SELECT COUNT(*) FROM OSS_Itembank.dbo.tblSetofAdminItems WHERE _fk_AdminSubject LIKE '%unit-test%'"
+        };
+        
         [TestInitialize]
         public void Setup()
         {
@@ -75,6 +95,12 @@ namespace TISUnitTests
 
                         updateConfigsCmd.ExecuteNonQuery();
                     }
+
+                    // Insert records into the OSS_TIS.dbo.CombinationTestMap table
+                    ExecuteInsertSqls(combinationTestMapSqls, connection);
+
+                    // Insert records into the OSS_TIS.dbo.CombinationTestFormMap table
+                    ExecuteInsertSqls(combinationTestFormMapSqls, connection);
                 }
 
                 trxScope.Complete();
@@ -92,6 +118,21 @@ namespace TISUnitTests
             using (var response = assessmentController.Delete(TEST_PACKAGE_KEY))
             {
                 Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["itembank"].ConnectionString))
+                {
+                    connection.Open();
+                    for (var i = 0; i < validationSqls.Length; i++)
+                    {
+                        using (var validationSqlCommand = new SqlCommand(validationSqls[i], connection))
+                        {
+                            validationSqlCommand.CommandType = CommandType.Text;
+                            int result = (int)validationSqlCommand.ExecuteScalar();
+
+                            Assert.AreEqual(0, result);
+                        }
+                    }
+                }
             }
         }
 
@@ -111,6 +152,25 @@ namespace TISUnitTests
                 });
 
                 loaderCommand.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Insert records into the <code>OSS_TIS.dbo.CombinationTestMap</code> table.
+        /// </summary>
+        /// <param name="insertSqls">The SQL necessary to insert the required records into the </param>
+        /// <param name="connection">The <code>SqlConnection</code> pointing to the database(s) that need to be loaded.</param>
+        /// <remarks>The process that loads assessments into TIS does not write records to these tables; they have to be created
+        /// manually.</remarks>
+        private void ExecuteInsertSqls(String[] insertSqls, SqlConnection connection)
+        {
+            for (var i = 0; i < insertSqls.Length; i++)
+            {
+                using (var combinationMapCommand = new SqlCommand(insertSqls[i], connection))
+                {
+                    combinationMapCommand.CommandType = CommandType.Text;
+                    combinationMapCommand.ExecuteNonQuery();
+                }
             }
         }
     }
