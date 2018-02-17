f86796b2c1c88481bedc49ec03de1c602af1443c
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-02-16 19:52:18 -0800

    TDS-1520: rename for consistency

---
+++ Common/TDSQASystemAPI/DAL/configs/LanguageDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace TDSQASystemAPI.DAL.configs
+{
+    /// <summary>
+    /// Represents a single record in the <code>OSS_Configs..Client_Language</code> table.
+    /// </summary>
+    /// /// <remarks>
+    /// This class is a subset of all the fields on the <code>OSS_Configs..Client_Language</code> table; fields that are not referenced by an INSERT/UPDATE 
+    /// in <code>OSS_Itembank.tp.spLoader_Main</code> (or the procedures it calls) aare not included.
+    /// </remarks>
+    public class Language
+    {
+        public string ClientName { get; set; }
+        public string LanguageName { get; set; } // Language
+        public string LanguageCode { get; set; }
+    }
+}
