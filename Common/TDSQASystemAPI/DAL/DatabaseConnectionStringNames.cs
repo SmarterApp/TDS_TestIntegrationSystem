11dd5ee42677b33373926178035d008f72fdef89
Jeff Johnson <jeffjohnson9046@hotmail.com>
2018-02-20 17:47:42 -0800

    TDS-1520: Set up for interacting with scoring db

--- Common/TDSQASystemAPI/DAL/DatabaseConnectionStringNames.cs
+++ Common/TDSQASystemAPI/DAL/DatabaseConnectionStringNames.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TDSQASystemAPI.DAL
+﻿namespace TDSQASystemAPI.DAL
 {
+    /// <summary>
+    /// The names of the various connection strings stored in the app.config file.
+    /// </summary>
     public static class DatabaseConnectionStringNames
     {
+        /// <summary>
+        /// Name of the connection string in app.config that points to the OSS_Configs database.
+        /// </summary>
         public const string CONFIGS = "configs";
+
+        /// <summary>
+        /// Name of the connection string in app.config that points to the OSS_Itembank database.
+        /// </summary>
         public const string ITEMBANK = "itembank";
+
+        /// <summary>
+        /// Name of the connection string in app.config that points to the OSS_TestScoringConfigs database.
+        /// </summary>
+        public const string SCORING = "scoring";
     }
 }
